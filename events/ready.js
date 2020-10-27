cd922a78352c7c355b46d073fda249bdf6c455c1
writemuhammad5 <70237123+writemuhammad5@users.noreply.github.com>
2020-10-27 14:34:35 -0500

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js")
+const { client, config } = require("../index.js")
+
+client.on("ready", () => {
+
+    console.log("|\n|    Mass DM Discord Bot\n|   Made by CRPT0#0001\n|\n| Last Update: 10/27/2020\n|")
+
+    client.user.setActivity(`+help for commands`, { type: "PLAYING" }).catch(console.error);
+
+})
